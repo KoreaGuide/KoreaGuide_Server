b2ca24cd2f3032d68d8196356545274c62c1e063
cindia3704 <cindia3704@gmail.com>
2021-02-28 19:49:26 +0900

    feat: JPA settings

---
+++ koreaguide-api/src/main/java/com/example/koreaguide/KoreaguideApplication.java
@@ -0,0 +1,13 @@
+package com.example.koreaguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreaguideApplication.class, args);
+    }
+
+}
---
+++ koreaguide-api/src/test/java/com/example/koreaguide/KoreaguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koreaguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
