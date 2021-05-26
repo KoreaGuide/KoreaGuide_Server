fcf4706584d54a4a03d19c228cc6607c85e67bdc
cindia3704 <cindia3704@gmail.com>
2021-05-26 21:00:10 +0900

    #30 파일 수정 기능 구현

--- koreaguide-api/src/main/java/com/example/koreaguide/repository/MapFileRepository.java
+++ koreaguide-api/src/main/java/com/example/koreaguide/repository/MapFileRepository.java
@@ -1,8 +1,13 @@
 package com.example.koreaguide.repository;
 
 import com.example.koreaguide.model.entity.MapFile;
+import com.example.koreaguide.model.entity.MyPlace;
+import com.example.koreaguide.model.entity.Place;
+import com.example.koreaguide.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /*
  * @author : Jisoo Kim
@@ -10,4 +15,6 @@ import org.springframework.stereotype.Repository;
 */
 @Repository
 public interface MapFileRepository extends JpaRepository<MapFile,Integer> {
+    MapFile getByUser(User user);
+
 }
--- koreaguide-api/src/main/java/com/example/koreaguide/service/MapFileApiLogicService.java
+++ koreaguide-api/src/main/java/com/example/koreaguide/service/MapFileApiLogicService.java
@@ -1,10 +1,44 @@
 package com.example.koreaguide.service;
 
+import com.example.koreaguide.model.entity.MapFile;
+import com.example.koreaguide.model.entity.User;
+import com.example.koreaguide.model.exception.KoreaGuideError;
+import com.example.koreaguide.model.exception.KoreaGuideException;
+import com.example.koreaguide.model.network.Header;
+import com.example.koreaguide.repository.MapFileRepository;
+import com.example.koreaguide.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 /*
  * @author : Jisoo Kim
  * @date: 2021/05/26 8:39 오후
 */
 @Service
 public class MapFileApiLogicService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    MapFileRepository mapFileRepository;
+
+    public Header storeFile(MultipartFile file, Integer userId) {
+        String fileName = file.getName();
+
+        Optional<User> user = userRepository.findById(userId);
+        Header ret = user.map(selectedUser->{
+            MapFile mapFile = mapFileRepository.getByUser(selectedUser);
+            mapFile.setFileName(fileName)
+                    .setUser(selectedUser)
+                    .setCreatedAt(LocalDateTime.now());
+            mapFileRepository.save(mapFile);
+            return Header.OK("File upload successful");
+        }).orElseThrow(()->new KoreaGuideException(KoreaGuideError.ENTITY_NOT_FOUND_USER));
+        return ret;
+    }
 }
